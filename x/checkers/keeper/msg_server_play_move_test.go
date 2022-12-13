0f2801c1ca2ff5d95257f15a75ab4ebbcdc0de4c
amazingify <mostafa_saeed@aucegypt.edu>
2022-12-13 11:39:46 +0200

    added moves tests

--- x/checkers/keeper/msg_server_play_move_test.go
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -15,6 +15,8 @@ import (
 func setupMsgServerWithOneGameForPlayMove(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
 	k, ctx := keepertest.CheckersKeeper(t)
 	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	// * this gets an object that can send
+	// * the messages in the system
 	server := keeper.NewMsgServerImpl(*k)
 	context := sdk.WrapSDKContext(ctx)
 	server.CreateGame(context, &types.MsgCreateGame{
@@ -26,7 +28,7 @@ func setupMsgServerWithOneGameForPlayMove(t testing.TB) (types.MsgServer, keeper
 	return server, *k, context
 }
 
-func TestPlayMov(t *testing.T) {
+func TestPlayMove(t *testing.T) {
 	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
 	playMoveResponse, err := msgServer.PlayMove(
 		context,
@@ -45,4 +47,130 @@ func TestPlayMov(t *testing.T) {
 		CapturedY: -1,
 		Winner:    "*",
 	}, *playMoveResponse)
+}
+
+func TestGameSavedCorrectly(t *testing.T) {
+
+	msgServer, keeper, context := setupMsgServerWithOneGameForPlayMove(t)
+	playMoveResponse, err := msgServer.PlayMove(
+		context,
+		&types.MsgPlayMove{
+			Creator:   bob,
+			GameIndex: "1",
+			FromX:     1,
+			FromY:     2,
+			ToX:       2,
+			ToY:       3,
+		})
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgPlayMoveResponse{
+		CapturedX: -1,
+		CapturedY: -1,
+		Winner:    "*",
+	}, *playMoveResponse)
+
+	game, found := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+	require.True(t, found)
+	require.EqualValues(t, game.Turn, "r")
+	require.EqualValues(t, game.Board, "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*")
+}
+
+func TestGameNotFound(t *testing.T) {
+	_, keeper, context := setupMsgServerCreateGame(t)
+	_, found := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "200")
+	require.False(t, found)
+}
+
+func TestSenderNotPlayer(t *testing.T) {
+	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
+	_, err := msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   alice,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	require.EqualValues(t, alice+": message creator is not a player", err.Error())
+}
+
+func TestMoveOutOfTurn(t *testing.T) {
+	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
+	_, err := msgServer.PlayMove(
+		context,
+		&types.MsgPlayMove{
+			Creator:   bob,
+			GameIndex: "1",
+			FromX:     1,
+			FromY:     2,
+			ToX:       2,
+			ToY:       3,
+		})
+	require.Nil(t, err)
+
+	_, err = msgServer.PlayMove(
+		context,
+		&types.MsgPlayMove{
+			Creator:   bob,
+			GameIndex: "1",
+			FromX:     1,
+			FromY:     2,
+			ToX:       2,
+			ToY:       3,
+		})
+	require.EqualValues(t, "{black}: player tried to play out of turn", err.Error())
+}
+
+func TestWrongMove(t *testing.T) {
+	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
+	_, err := msgServer.PlayMove(
+		context,
+		&types.MsgPlayMove{
+			Creator:   bob,
+			GameIndex: "1",
+			FromX:     1,
+			FromY:     2,
+			ToX:       0,
+			ToY:       0,
+		})
+	require.EqualValues(t, "Invalid move: {1 2} to {0 0}: wrong move", err.Error())
+}
+
+func Test2MovesSavedGame(t *testing.T) {
+
+	msgServer, keeper, context := setupMsgServerWithOneGameForPlayMove(t)
+	msgServer.PlayMove(context,
+		&types.MsgPlayMove{
+			Creator:   bob,
+			GameIndex: "1",
+			FromX:     1,
+			FromY:     2,
+			ToX:       2,
+			ToY:       3,
+		})
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   carol,
+		GameIndex: "1",
+		FromX:     0,
+		FromY:     5,
+		ToX:       1,
+		ToY:       4,
+	})
+
+	systemInfo, found := keeper.GetSystemInfo(sdk.UnwrapSDKContext(context))
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId: 2,
+	}, systemInfo)
+
+	game1, found := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+	require.True(t, found)
+	require.EqualValues(t, types.StoredGame{
+		Index: "1",
+		Board: "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|*r******|**r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:  "b",
+		Black: bob,
+		Red:   carol,
+	}, game1)
+
 }
